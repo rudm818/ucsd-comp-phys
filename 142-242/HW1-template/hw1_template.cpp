4aa2939f6dd140f628b76a5ec2f1a208f617067a
mmfolkerts <mmfolkerts@gmail.com>
2012-04-24 02:23:27 +0000

    updated template

--- 142-242/HW1-template/hw1_template.cpp
+++ 142-242/HW1-template/hw1_template.cpp
@@ -11,7 +11,7 @@
 
 //blas
 //#include <cblas.h>
-
+#include <math.h>
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -22,78 +22,87 @@ using std::ofstream;
 #include <algorithm>
 using std::fill;
 
-//allow complex numbers
+//allow complex numbers (from "our" library)
 #include "../complexObject/complex.h"
 
 //include function definitions
 #include "templateFunctions.cpp"
 
 //change below to switch between double and float
-#define FP float
+#define FLOAT float
+
+//__CONSTANTS__
+FLOAT PI = acos(-1.0);
+FLOAT PLANCKS = 1.0;//Planck's constant
+FLOAT MASS   = 1.0;//mass of particle
+
 
 //__PARAMETERS__
 
 //defines length of wave-funtion potential
-int X_DIM = 10;
+const int X_DIM = 600;
 
 //wave function parameters
-FP SIGMA = 1.0f;
+const FLOAT SIGMA = 1.0f;
+const FLOAT X0 = 0.75f;
+const FLOAT NORM = pow(2.0/PI,0.25);
+
+//define potential function (used for Legrangian = KE - PotentialFxn)
+FLOAT PotentialFxn( FLOAT x){
+	return pow(x,2.0f)/2.0f; //1D harmonic oscilator
+}
 
 //defines x-axis from - X_RANGE_LOW to + X_RANGE_HI
-FP X_RANGE_LOW = -1.0f;
-FP X_RANGE_HI = 1.0f;
-
+const FLOAT X_RANGE_LOW = -4.0f;
+const FLOAT X_RANGE_HI  =  4.0f;
 
 //integration factors and limits
-FP DELTA_X = 0.01f;
-int K_POWER = 4;
+const FLOAT DELTA_T = 0.01f;
+const int K_POWER =  4; //the power of the aggregate KeN = (Kepsilon)^K_POWER
+const int MAX_K_OPS = 100; // totalTime = K_POWER*DELTA_T * MAX_K_OPS
 
-MAX_K_OPS = 100;
 
+//__main function__
+int main(int argc, char* argv[]){
 
-//define the potentail in a function
-FP potential(FP x){
-	return x*x;	
-}
-
-FP waveFxn(FP position){
-	FP sigmaSquared = SIGMA*SIGMA; //whatever you want really
-	return exp((x-X0)*(x-X0)/sigmaSquared);
-}
-
-int main(int argc, char* argc[]){
+	//sanity check
+	if( 2*sizeof(FLOAT) != sizeof(complex<FLOAT>) ){
+		cout << "ERROR, complex structure is bad!\n";
+		exit(1);
+	}
 
 	//create and fill descretized x-axis:
-	FP* xAxis = new FP[X_DIM];
-	fillRange(xAxis, X_RANGE_LOW, X_RANGE_HI, X_DIM);
-
+	FLOAT* xAxis = new FLOAT[X_DIM];
+	FLOAT deltaX = fillRange(xAxis, X_RANGE_LOW, X_RANGE_HI, X_DIM);
 
 	//create and fill K-epsilon matrix
-	complex<FP>* Ke = new complex<FP>[X_DIM][X_DIM];
-	fillPropagator1D(Ke,xAxis,DELTA_X,potential,X_DIM); //potential should be a FUNCTION that returns the potential as a function of position
+	complex<FLOAT>* Ke = (complex<FLOAT>*)malloc(sizeof(complex<FLOAT>)*X_DIM*X_DIM); //malloc();
+	fillPropagator1D(Ke,deltaX,DELTA_T,PLANCKS,MASS,PotentialFxn,xAxis,X_DIM); //potential should be a FUNCTION that returns the potential as a function of position
 	
 	//create an aggregate K matrix
-	complex<FP>* KeN = new complex<FP>[X_DIM][X_DIM];
-	cSingleSqMatMatMult(KeN, Ke, Ke, X_DIM);
-	for (int n=1; n<K_POWER; n++) {
+	complex<FLOAT>* KeN = (complex<FLOAT>*)malloc(sizeof(complex<FLOAT>)*X_DIM*X_DIM);
+	cSingleSqMatMatMult(KeN, Ke, Ke, X_DIM);//Ke^2
+	for (int n=2; n<K_POWER; n++) { //Ke^K_POWER (dX^N term is already added)
 		cSingleSqMatMatMult(KeN, KeN, Ke, X_DIM);
 	}
 	
 	//create and fill waveFxn
-	complex<FP>* psi = new complex<FP>[X_DIM];
-	fillWaveFxn(psi,waveFxn,X_DIM); //fills and normalizes wave function
-			
+	complex<FLOAT>* psi = (complex<FLOAT>*)malloc(sizeof(complex<FLOAT>)*X_DIM); //empty
+	fillGaussianFxn(psi,NORM,X0,SIGMA,xAxis,X_DIM); //fills initial wave function
+		
 	for (int timeStep = 0; timeStep<MAX_K_OPS; timeStep++) {
 		//multiply wave function by KeN (propegate)
 		cSingleSqSymMatVectMult(psi,KeN,psi,X_DIM);
 		
 		//then compute expectation values of x,v,potE,kinE,totE ...
+		//computeExpectation of x
+		cout << "<x>:" << expectationX(psi,xAxis,deltaX,X_DIM) << endl;	
 		//save data to file...
 	}
 	
 	//clean up memory
 	delete xAxis;
-	delete Ke;
-	delete KeN;
-	delete psi;
+	free(Ke);
+	free(KeN);
+	free(psi);
 }
--- 142-242/HW1-template/templateFunctions.cpp
+++ 142-242/HW1-template/templateFunctions.cpp
@@ -6,28 +6,58 @@
 //functions used in hw1_template.cpp
 
 template <class floatType>
-void fillRange(floatType* Axis, floatType LOW, floatType HI, int DIM){
-	
+floatType fillRange(floatType* Axis, floatType LOW, floatType HI, int DIM){
 	floatType delta = (HI-LOW)/float(DIM-1); //note the '-1' is required to fill whole range
 	for(int i=0; i<DIM; i++){
 		Axis[i] = LOW + floatType(i)*delta;
 	}
+	return delta;
 }
 
 
 //here we are using a funtion as a parameter
 //described here: http://stackoverflow.com/questions/9410/how-do-you-pass-a-function-as-a-parameter-in-c
 template <class floatType>
-void fillPropagator1d(complex<floatType>* K,floatType* Xpos,floatType dX, floatType (*V)(floatType),int DIM){
+void fillPropagator1D(complex<floatType>* K,,floatType dX, floatType dT,floatType h, floatType m, floatType (*V_Fxn)(floatType),floatType* Xpos, int DIM){
 	//some propagator code...
-	//floatType a = (*V)(Xpos[i])
+	complex<floatType> A;
+	A.a = 0.0f;//real part
+	A.b = 1.0f;//complex part UPDATE THIS!!
+	for (int i=0; i<DIM; i++) {
+		for (int j=0; j<DIM; j++) {
+			floatType argument = dT/h*( m/2.0 * pow((xj-xi)/dT,2.0) - (*V_Fxn)( (xj+xi)/2.0 ) ) ;
+			//compute complex number exp[i * arg] = cos(arg) + i*sin(arg)
+			complex<floatType> Kelement;
+			complex<floatType> preFactor = cDiv(dX,A);
+			Kelement.a = preFactor.a*cos(argument);//real
+			Kelement.b = preFactor.b*sin(argument);//complex part
+			
+			K[i][j]= Kelement; //dX is added ALREADY see notes...
+		}
+	}
 }
 
-//fills and normalizes wave function
+//computes normalized initial wave function
 template <class floatType>
-fillWaveFxn(complex<floatType>* psi,int DIM){
-	//do it yourself
-	//also normalize
+void fillGaussianFxn(complex<floatType>* psi,floatType norm, floatType x0, floatType sigma, FLOAT* xAxis, int DIM){
+	
+	FLOAT sigmaSquared = sigma*sigma; //whatever you want really
+	for(int i=0; i<DIM; i++){
+		psi[i].a = norm*exp(pow(xAxis[i]-x0,2.0)/sigmaSquared);
+		psi[i].b = 0.0;
+	}
+	
 }
+
+FLOAT expectationX(complex<FLOAT>* psi,FLOAT* xAxis,FLOAT dX, int DIM){
+	
+	//<x> = integrate[conj(psi) x psi]
+	FLOAT sum = 0.0f;
+	
+	for (int i=0; i<DIM; i++) {
+		sum += dX*xAxis[i]*(psi[i].a*psi[i].a+psi[i].b*psi[i].b);//x[i]*Conj(psi)*psi = x[i]*cMag(psi)^2
+	}
+}
+
 
 #endif
